5181618c2cfbb7828c431c7c94cd0610e95aa51d
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2021-11-03 07:07:40 +0800

    feat: add new docker images and fix scripts

--- scripts/process_all.py
+++ scripts/process_all.py
@@ -3,7 +3,12 @@ import subprocess
 import sys
 
 environments = [
-    {"NAME": "latest", "PYTHON_VERSION": "3.8"},   
+    {"NAME": "latest", "PYTHON_VERSION": "3.9"},   
+    
+    {"NAME": "python3.9", "PYTHON_VERSION": "3.9"},
+    {"NAME": "python3.9-debian-10", "PYTHON_VERSION": "3.9"},
+    {"NAME": "python3.9-prod", "PYTHON_VERSION": "3.9"},
+    {"NAME": "python3.9-prod-debian-10", "PYTHON_VERSION": "3.9"},
     
     {"NAME": "python3.8", "PYTHON_VERSION": "3.8"},
     {"NAME": "python3.8-debian-10", "PYTHON_VERSION": "3.8"},
